95473c9a7c6a9d6e92ad6ba93544150a6863e0f7
ChanningC12 <chicheng2015@u.northwestern.edu>
2016-03-07 17:24:25 -0600

    Logistic regression example

--- PythonDataAnalysis/Logistic Regression Example.py
+++ PythonDataAnalysis/Logistic Regression Example.py
@@ -68,6 +68,94 @@ plt.xlabel('Years Married')
 plt.ylabel('Percentage')
 
 
+# Prepare Data for Logistic Regression
+y, X = dmatrices('affair ~ rate_marriage + age + yrs_married + children + \
+                  religious + educ + C(occupation) + C(occupation_husb)',
+                  dta, return_type = "dataframe")
+print X.columns
+
+X = X.rename(columns = {'C(occupation)[T.2.0]':'occ_2',
+                        'C(occupation)[T.3.0]':'occ_3',
+                        'C(occupation)[T.4.0]':'occ_4',
+                        'C(occupation)[T.5.0]':'occ_5',
+                        'C(occupation)[T.6.0]':'occ_6',
+                        'C(occupation_husb)[T.2.0]':'occ_husb_2',
+                        'C(occupation_husb)[T.3.0]':'occ_husb_3',
+                        'C(occupation_husb)[T.4.0]':'occ_husb_4',
+                        'C(occupation_husb)[T.5.0]':'occ_husb_5',
+                        'C(occupation_husb)[T.6.0]':'occ_husb_6'})
+print X.columns
+print y
+print X
+
+# flatten y into 1-D array
+y = np.ravel(y)
+
+# Logistic Regression
+## instantiate a logistic regression model and fit with X and y
+model = LogisticRegression()
+model = model.fit(X,y)
+
+# check the accuracy on the training set
+model.score(X, y)
+
+# what percentage had affairs
+y.mean()
+
+# examine the coefficients
+pd.DataFrame(zip(X.columns,np.transpose(model.coef_)))
+
+# model evaluation using a validation set
+# evaluate the model by splitting into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=0)
+model2 = LogisticRegression()
+model2.fit(X_train, y_train)
+
+# predict class labels for the test set
+predicted = model2.predict(X_test)
+print predicted
+
+# generate class probabiliies
+probs = model2.predict_proba(X_test)
+print probs
+
+# generate evaluation metrics
+print metrics.accuracy_score(y_test,predicted)
+print metrics.roc_auc_score(y_test,probs[:,1])
+
+# model evaluation using cross-validation
+# evaluate the model using 10-fold cross-validation
+scores = cross_val_score(LogisticRegression(),X,y,scoring='accuracy',cv=10)
+print scores
+print scores.mean()
+
+# predicting the probability of an Affair
+model.predict_proba(np.array([1,0,0,1,0,0,1,0,0,0,0,3,25,3,1,4,16]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
